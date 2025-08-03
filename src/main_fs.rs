e784c023899ca83c636054d4162f72a3ed1697a6
NaKeR <122722654+NaKeRNarolino@users.noreply.github.com>
2025-08-03 22:19:51 +0400

    Mosaic 0.0.2

--- src/main_fs.rs
+++ src/main_fs.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use subprocess::Exec;
+use subprocess::{Exec, NullFile};
 use crate::web;
 
 pub fn create_required_folders() -> anyhow::Result<()> {
@@ -13,19 +13,21 @@ pub async fn download(name: String) -> anyhow::Result<()> {
 
     let index = web::get_index().await?;
 
-    dbg!(&index);
+    // dbg!(&index);
 
     let p = index.get(&name).unwrap();
 
+    let _ = fs::remove_dir_all(format!("./mosaic_libs/{name}"));
 
     let e = Exec::cmd("git").arg("clone").arg(
         &p.git
     ).arg(format!("./mosaic_libs/{name}"));
 
-    e.capture()?;
+    e.stdout(NullFile).capture()?;
 
-    let e = Exec::cmd("rm").arg("-rf").arg(format!("./mosaic_libs/{name}/.git"));
-    e.capture()?;
+    // let e = Exec::cmd("rm").arg("-rf").arg(format!("./mosaic_libs/{name}/.git"));
+    // e.capture()?;
+    let _ = fs::remove_dir_all(format!("./mosaic_libs/{name}/.git"));
 
     Ok(())
 }
